864997d38b9ab0877bdd72675940d2f2a88d59a8
Gauthier Painteaux <gauthier.painteaux@gmail.com>
2024-01-09 21:55:34 +0100

    Update metadata and add JSON-LD script

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Provider from './Provider'
 export const metadata: Metadata = {
   title: 'Gauthier Painteaux · Portfolio',
   description: 'Gauthier Painteaux, développeur web fullstack basé à Reims, France.',
-  applicationName: 'Gauthier Painteaux · Portfolio',
+  applicationName: 'Gauthier Painteaux',
   keywords: 'Gauthier Painteaux, Painteaux, Développeur Web, Développeur Web Fullstack',
   authors: { name: 'Gauthier Painteaux', url: 'https://gauthierpainteaux.fr' },
   creator: 'Gauthier Painteaux',
@@ -13,8 +13,20 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: 'Gauthier Painteaux',
+    alternateName: 'GP',
+    url: 'https://gauthierpainteaux.fr/',
+  }
+
   return (
     <html lang="en">
+      <head>
+        {/* eslint-disable-next-line react/no-danger */}
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
+      </head>
       <body>
         <main>
           <Provider>{children}</Provider>
