d27ab0fa0b1ac4741733568dfc7e793f3c5fe2c2
Gauthier Painteaux <gauthier.painteaux@koul.io>
2026-02-03 15:27:55 +0100

    Config - #PRT-NoId - Update dependencies

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  reactCompiler: true,
   experimental: {
     optimizePackageImports: ['@chakra-ui/react', 'react-icons/io'],
   },
